58e0ec52a405a221c6787592a5ef68f3a8aead4a
Matt Eisenberg <eisenbergm@wit.edu>
2017-02-23 01:53:24 -0500

    Bottombar layout looks decent now

--- app/src/main/java/edu/wit/mobileapp/pocketstudio/ProjectEditor.java
+++ app/src/main/java/edu/wit/mobileapp/pocketstudio/ProjectEditor.java
@@ -13,49 +13,13 @@ import android.widget.Toast;
 import com.roughike.bottombar.*;
 
 public class ProjectEditor extends AppCompatActivity {
-    private MediaPlayer mediaPlayer;
-    private MediaRecorder recorder;
-    private String AUDIO_FILE;
-    private String currentToast;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_editor);
 
-        AUDIO_FILE= Environment.getExternalStorageDirectory()+"/audiorecorder.3gpp";
-
-        Toast currentToast;
-        setupBottomBar();
 
     }
 
-
-    private void setupBottomBar(){
-        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
-        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
-            @Override
-            public void onTabSelected(@IdRes int tabId){
-                if (tabId == R.id.tab_record){
-                    Log.i("recordTap", "Record button tapped");
-
-                    currentToast = null;
-                    Toast currentToast = Toast.makeText(ProjectEditor.this, "Recording begun...", Toast.LENGTH_SHORT
-                    );
-                    currentToast.show();
-                }
-                 else if (tabId == R.id.tab_play){
-                    Log.i("playTap", "Play button tapped");
-                }
-                else if (tabId == R.id.tab_backzero){
-                    Log.i("backZeroTap", "Back to zero");
-                }
-            }
-        });
-    }
-
-    public void buttonTapped(View view){
-
-    }
 }
