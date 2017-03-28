74cb9654ca769a5eb16d8300003b66b00f6f29a1
Matt Eisenberg <eisenbergm@wit.edu>
2017-03-28 15:37:35 -0400

    Test login page

--- app/src/main/java/edu/wit/mobileapp/pocketstudio/projectsFragment.java
+++ app/src/main/java/edu/wit/mobileapp/pocketstudio/projectsFragment.java
@@ -1,5 +1,6 @@
 package edu.wit.mobileapp.pocketstudio;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -30,6 +32,17 @@ public class ProjectsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View myInflatedView = inflater.inflate(R.layout.projects_fragment,container,false);
+
+        //    START MAKING PAGE BUTTON
+        final Button loginPageButton = (Button)myInflatedView.findViewById(R.id.loginTestPageButton);
+        loginPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginPage = new Intent(getActivity(), LoginActivity.class);
+                startActivity(loginPage);
+                Log.d(TAG, "Definitely pressed that button");
+            }
+        });
 
         //MY PROJECTS SECTION
         ListView projectsListView = (ListView) myInflatedView.findViewById(R.id.myProjectsListview);
