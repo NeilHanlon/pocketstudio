fcbd81159956fe2b5ffafbff2535d5800e1587fc
Neil Hanlon <neil@neilhanlon.com>
2017-04-08 12:32:58 -0400

    Finishing up Settings Fragment

--- app/src/main/java/edu/wit/mobileapp/pocketstudio/SettingsFragment.java
+++ app/src/main/java/edu/wit/mobileapp/pocketstudio/SettingsFragment.java
@@ -1,22 +1,40 @@
 package edu.wit.mobileapp.pocketstudio;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.RelativeLayout;
 
 /**
  * Created by Neil on 4/8/2017.
  */
 
 public class SettingsFragment extends Fragment {
+
+    RelativeLayout mMainContent;
+    FragmentTransaction ft;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings,container,false);
+        RelativeLayout notiRow = (RelativeLayout) view.findViewById(R.id.notificationLayoutBox);
+
+        notiRow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mMainContent = (RelativeLayout) view.findViewById(R.id.mainContent);
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.mainContent, new NotificationPrefFragment())
+                .commit();
+            }
+        });
         return view;
     }
 }
